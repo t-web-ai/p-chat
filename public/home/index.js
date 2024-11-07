3704341a701d2f060ebb474cb9704026d8799814
Toee Thiha Kyaw <sanjikun823@gmail.com>
2024-11-07 16:05:04 +0630

    Initialize DOM elements.

--- public/home/index.js
+++ public/home/index.js
@@ -1,1 +1,7 @@
 const socket = new io();
+let username = document.forms[0].children[0];
+let promptBox = document.querySelector(".prompt-name");
+let onlineCount = document.querySelector(".online-users-count");
+let messageBox = document.querySelector(".message-box");
+let typing = document.querySelector(".typing");
+let msg = document.forms[1].children[0];
